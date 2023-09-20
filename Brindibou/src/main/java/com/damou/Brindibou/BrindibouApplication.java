b8d12e1099f27fbb72f26bcd0b28566693600b52
moulindavid <moulin.david@live.fr>
2023-09-20 17:22:38 +0200

    feat: init project with postgres docker

---
+++ Brindibou/src/main/java/com/damou/Brindibou/BrindibouApplication.java
@@ -0,0 +1,13 @@
+package com.damou.Brindibou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrindibouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrindibouApplication.class, args);
+	}
+
+}
---
+++ Brindibou/src/test/java/com/damou/Brindibou/BrindibouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damou.Brindibou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrindibouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
